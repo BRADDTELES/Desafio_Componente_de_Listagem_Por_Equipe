998008f95aebdd3f169f1c79172b8891ddf4dbd4
BRADDTELES <braddteles@gmail.com>
2025-08-26 16:43:22 -0300

    style: ajuste no item da lista, no icone da seta para direita de Navegação.

--- app/src/main/java/com/equipe4/desafiocomponentedelistagem/adapter/CategoriaAdapter.kt
+++ app/src/main/java/com/equipe4/desafiocomponentedelistagem/adapter/CategoriaAdapter.kt
@@ -1,15 +1,19 @@
 package com.equipe4.desafiocomponentedelistagem.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.equipe4.desafiocomponentedelistagem.R
 import com.equipe4.desafiocomponentedelistagem.databinding.ItemCategoriasPerfilBinding
 import com.equipe4.desafiocomponentedelistagem.diffcallback.CategoriaDiffCallback
 import com.equipe4.desafiocomponentedelistagem.model.Categorias
 
 class CategoriaAdapter(
-
+    private val onClick: (Categorias) -> Unit
 ) : ListAdapter<Categorias, CategoriaAdapter.CategoriasViewHolder>(
     CategoriaDiffCallback()
 ) {
@@ -30,9 +34,21 @@ class CategoriaAdapter(
     }
 
     inner class CategoriasViewHolder(private val binding: ItemCategoriasPerfilBinding) : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(categoria: Categorias) {
+            Glide.with(binding.imgIconeCategoria).load(categoria.icone).centerCrop().placeholder(R.drawable.fundo_perfil).into(binding.imgIconeCategoria)
             binding.tvNomeCategoria.text = categoria.nome
             binding.tvDescricaoCategoria.text = categoria.descricao
+
+            if (categoria.isNotification == true){
+                binding.imageView2.visibility = View.VISIBLE
+                binding.tvNotificationCategoria.text = "%.0f".format(categoria.qtdNotification)
+            } else {
+                binding.imageView2.visibility = View.GONE
+                binding.tvNotificationCategoria.text = ""
+            }
+
+            binding.imgNavegar.setOnClickListener { onClick(categoria) }
         }
     }
 }

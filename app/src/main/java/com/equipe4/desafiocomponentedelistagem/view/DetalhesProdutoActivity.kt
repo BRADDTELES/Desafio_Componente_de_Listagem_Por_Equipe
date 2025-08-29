7c3a80382c2344f1c71f699aae997d0c36414316
eacosta1956 <eldon.costa@terra.com.br>
2025-08-29 01:38:44 -0300

    feat(activity): implementa DetalhesRestauranteActivity e placeholder da DetalhesProdutoActivity para teste de navegação

---
+++ app/src/main/java/com/equipe4/desafiocomponentedelistagem/view/DetalhesProdutoActivity.kt
@@ -0,0 +1,21 @@
+package com.equipe4.desafiocomponentedelistagem.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.equipe4.desafiocomponentedelistagem.R
+
+class DetalhesProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes_produtos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

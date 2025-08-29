371f108179bd7a96b651d133257ed46eb59e96bc
Lena <elenasmclemente@gmail.com>
2025-08-29 13:35:08 -0300

    fix: corrigido o conflito na Feature/detalhes_produto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)

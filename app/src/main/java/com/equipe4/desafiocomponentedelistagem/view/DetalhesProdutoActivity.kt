7f8c85f30c08ccc6368852f8eb05c08966488dac
Lena <elenasmclemente@gmail.com>
2025-08-31 14:02:57 -0300

    style: modificar cor do FAB

--- app/src/main/java/com/equipe4/desafiocomponentedelistagem/view/DetalhesProdutoActivity.kt
+++ app/src/main/java/com/equipe4/desafiocomponentedelistagem/view/DetalhesProdutoActivity.kt
@@ -71,7 +71,7 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         val listaOpcionais= listOf(
             Opcional("Picles","teste",10.0,R.drawable.picles,0),
             Opcional("Queijo","Incremente seu lanche com esse sabor",5.0,R.drawable.queijo,0),
-            Opcional("onion rings","Experimente",18.0,R.drawable.onion_rings,0),
+            Opcional("Onion rings","Experimente",18.0,R.drawable.onion_rings,0),
             Opcional("Suco de morango","É uma delicoa",12.0,R.drawable.suco_morango,0),
             Opcional("Batata frita","Incremente seu lanche com esse sabor",5.0,R.drawable.batata_frita1,0),
             Opcional("Refri","Refrescante",9.0,R.drawable.refri1,0),
